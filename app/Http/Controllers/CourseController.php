eff5173a4a629533736f97102c23c84fba6ee4c8
kennethprz09 <kennethprz09@gmail.com>
2026-02-04 12:29:53 -0400

    correcciones y mejoras update

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -118,8 +118,48 @@ class CourseController extends Controller
     public function update(CourseStoreRequest $request, $id)
     {
         return $this->runTransaction(function () use ($request, $id) {
-            $post = $request->except(['start_date']);
-            $data = $this->courseRepository->store($post, $id);
+            $courseBook = CourseBook::where('course_id', $id)->first();
+            $courseDay = CourseDay::where('course_id', $id)->first();
+            $discount = Discount::where('day_course_id', $courseDay?->id)->first();
+
+            $course = $this->courseRepository->store([
+                'id' => $request->id,
+                'name' => $request->name,
+                'level_id' => $request->level_id,
+                'period_id' => $request->period_id
+            ]);
+
+            if ($request->book_id) {
+                $book = $this->courseBookRepository->store([
+                    'id' => $courseBook->id,
+                    'book_id' => $request->level_id,
+                    'course_id' => $course->id
+                ]);
+            }
+            
+            $days = $this->courseDayRepository->store([
+                'id' => $courseDay->id,
+                'time' => $request->time,
+                'modalidad' => $request->modalidad,
+                'course_init' => $request->course_init,
+                'price' => $request->price,
+                'orden' => $request->orden,
+                'periodo' => $request->periodo,
+                'status' => $request->status,
+                'day_id' => $request->day_id,
+                'course_id' => $course->id
+            ]);
+
+            $discount = $this->discountRepository->store([
+                'id' => $discount->id,
+                'start_date' => $request->start_date,
+                'finish_date' => $request->finish_date,
+                'discount' => $request->discount,
+                'payback' => 0,
+                'link' => $request->link,
+                'link_book' => $request->link_book,
+                'day_course_id' => $days->id,
+            ]);
 
             return [
                 'code' => 200,
@@ -138,8 +178,10 @@ class CourseController extends Controller
                 // Borra el libro
                 CourseBook::where('course_id', $id)->delete();
                 
-                // Busca el CourseDay
+                // Busca y cambiar el estado de CourseDay
                 $courseDay = CourseDay::where('course_id', $id)->first();
+                $courseDay->status = "Finalizado";
+                $courseDay->save();
 
                 // Elimina el descuento
                 Discount::where('day_course_id', $courseDay?->id)->delete();
